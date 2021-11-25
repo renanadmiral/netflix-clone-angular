43fc3b9c623193220e521ce75a9222af77696a04
Everton Reis <55769021+evertoont@users.noreply.github.com>
2021-11-24 22:01:14 -0300

    feat: create layout login screen

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  footerLinks: string[] = [
+    'perguntas frequentes', 'centro de ajuda', ' termos de uso', 'privacidade',
+    'preferências de cookies', 'informações corporativas'
+  ];
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  handleLogin(): void {
+    localStorage.setItem('token', '1efefwgoidfhqoi')
+    this.router.navigate(['/home']);
   }
 
 }
